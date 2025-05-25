bb47e6b776d4e68443bed8172a2ca4143c1d1063
VinceCYLiao <lounger23@gmail.com>
2025-05-26 06:39:17 +0800

    assignment 1 done

--- assignment1/src/main.cpp
+++ assignment1/src/main.cpp
@@ -8,6 +8,7 @@
 #include <string>
 #include <cmath>
 #include <chrono>
+#include <thread>
 
 using namespace std;
 
@@ -17,11 +18,18 @@ const double kattract = 0.001;
 
 const double krepel = 0.001;
 
-std::string promptForFilename() {
-    std::cout << "Enter the filename : " << std::endl;
-    std::string filename {};
-    std::getline(std::cin, filename);
-    return filename;
+std::ifstream promptForIfstream() {
+    while(true) {
+        std::cout << "Enter the filename : " << std::endl;
+        std::ifstream ifs;
+        std::string filename {};
+        std::getline(std::cin, filename);
+        ifs.open(filename);
+        if(!ifs.fail()) {
+            return ifs;
+        }
+        std::cout << "File does not exist." << std::endl;
+    }
 }
 
 int promptForPositiveInt() {
@@ -31,7 +39,7 @@ int promptForPositiveInt() {
         std::string input;
         std::getline(std::cin, input);
         std::stringstream ss(input);
-        if(ss >> i) {
+        if(ss >> i && i > 0) {
             return i;
         }
         std::cout << "Input is not valid. Try again." << std::endl;
@@ -39,44 +47,75 @@ int promptForPositiveInt() {
     }
 }
 
-
-std::vector<string> split(stringstream& ss, char deli) {
-    std::vector<string> sv{};
-    for (std::string line; std::getline(ss, line, deli);) {
-        sv.push_back(line);
+SimpleGraph initSimpleGraph(std::ifstream& ifs) {
+    const double kPi = 3.14159265358979323;
+    SimpleGraph sg{};
+    int nodeNumber;
+    ifs >> nodeNumber;
+    sg.nodes.resize(nodeNumber);
+    size_t start, end;
+    while(ifs >> start >> end) {
+        sg.edges.push_back(Edge{ start, end });
     }
-    return sv;
+    for(int i = 0; i < nodeNumber; ++i) {
+        sg.nodes[i] = {
+            .x = cos(2*kPi*i/nodeNumber),
+            .y = sin(2*kPi*i/nodeNumber),
+        };
+    }
+    return sg;
 }
 
-void updateNodePositionForAttraction(SimpleGraph& sg) {
-    for (size_t i = 0; i < sg.edges.capacity(); ++i) {
+
+void updateDeltaForAttraction(SimpleGraph& sg, std::vector<std::pair<double, double>>& delta) {
+    for (size_t i = 0; i < sg.edges.size(); ++i) {
         auto& [e1, e2] = sg.edges.at(i);
         auto& node1 = sg.nodes.at(e1);
         auto& node2 = sg.nodes.at(e2);
         auto [x0, y0] = node1;
         auto [x1, y1] = node2;
-        double fattract = kattract * ((y1 - y0) * (y1 - y0) + (x1 - x0) * (x1 - x0));
-        double theta = atan2(y1 - y0, x1 - x0);
-        node1.x -= fattract * cos(theta);
-        node1.y -= fattract * sin(theta);
-        node2.x += fattract * cos(theta);
-        node2.y += fattract * sin(theta);
+        auto dy = y1 - y0;
+        auto dx = x1 - x0;
+        double fattract = kattract * (dy * dy + dx * dx);
+        double theta = atan2(dy, dx);
+        delta[e1].first += fattract * cos(theta);
+        delta[e1].second += fattract * sin(theta);
+        delta[e2].first -= fattract * cos(theta);
+        delta[e2].second -= fattract * sin(theta);
     }
 }
 
-void updateNodePositionForRepulsive(SimpleGraph& sg) {
-    for(size_t i = 0; i < sg.nodes.capacity(); ++i) {
-        auto& node1 = sg.nodes.at(i);
-        auto node2Index = i+1;
-        auto& node2 = sg.nodes.at(node2Index == sg.nodes.capacity() ? 0 : node2Index);
-        auto [x0, y0] = node1;
-        auto [x1, y1] = node2;
-        double frepel = krepel / sqrt((y1 - y0) * (y1 - y0) + (x1 - x0) * (x1 - x0));
-        double theta = atan2(y1 - y0, x1 - x0);
-        node1.x -= frepel * cos(theta);
-        node1.y -= frepel * sin(theta);
-        node2.x += frepel * cos(theta);
-        node2.y += frepel * sin(theta);
+void updateDeltaForRepulsive(SimpleGraph& sg, std::vector<std::pair<double, double>>& delta) {
+    for(size_t i = 0; i < sg.nodes.size(); ++i) {
+        for (size_t j = i+1; j < sg.nodes.size(); ++j) {
+                auto& node1 = sg.nodes.at(i);
+                auto& node2 = sg.nodes.at(j);
+                auto [x0, y0] = node1;
+                auto [x1, y1] = node2;
+                auto dy = y1 - y0;
+                auto dx = x1 - x0;
+                double distance = sqrt(dy * dy + dx * dx);
+                if(distance > 0.000001) {
+                    double frepel = krepel / distance;
+                    double theta = atan2(dy, dx);
+                    delta[i].first -= frepel * cos(theta);
+                    delta[i].second -= frepel * sin(theta);
+                    delta[j].first += frepel * cos(theta);
+                    delta[j].second  += frepel * sin(theta);
+                }
+        }
+
+    }
+}
+
+
+void updatePosition(SimpleGraph& sg) {
+    std::vector<std::pair<double, double>> delta(sg.nodes.size(), {0.0, 0.0});
+    updateDeltaForAttraction(sg, delta);
+    updateDeltaForRepulsive(sg, delta);
+    for(size_t i = 0; i < sg.nodes.size(); ++i) {
+        sg.nodes[i].x += delta[i].first;
+        sg.nodes[i].y += delta[i].second;
     }
 }
 
@@ -84,57 +123,34 @@ void updateNodePositionForRepulsive(SimpleGraph& sg) {
 // Main method
 int main() {
     Welcome();
-    int timeLimit = promptForPositiveInt();
-    std::ifstream i;
-    while (true) {
-        std::string filename { promptForFilename() };
-        i.open(filename);
-        if (!i.fail()) {
-            break;
+    while(true) {
+        int timeLimit { promptForPositiveInt() };
+        std::ifstream i { promptForIfstream() };
+        SimpleGraph sg { initSimpleGraph(i) };
+
+        InitGraphVisualizer(sg);
+        DrawGraph(sg);
+        while(timeLimit > 0) {
+            auto start = std::chrono::high_resolution_clock::now();
+
+            updatePosition(sg);
+
+            auto end = std::chrono::high_resolution_clock::now();
+            auto elapsed = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+            int microseconds = elapsed.count();
+            timeLimit -= microseconds;
+            DrawGraph(sg);
+            std::this_thread::sleep_for(std::chrono::microseconds(100));
         }
-
+        std::cout << "Do you want to try another file ?" << std::endl;
+        std::string answer;
+        std::getline(std::cin, answer);
+        if(answer == "yes" || answer == "y") {
+            continue;
+        }
+        std::cout << "Bye." << std::endl;
+        break;
     }
-    std::stringstream ss;
-    ss << i.rdbuf();
-    auto sv = split(ss, '\n');
-    int nodeNumber = std::stoi(sv.at(0));
-    SimpleGraph sg {};
-    std::vector<Node> nv{};
-    const double kPi = 3.14159265358979323;
-    for(int i = 0; i <= nodeNumber; ++i) {
-        nv.push_back({
-            .x = cos(2*kPi*i/nodeNumber),
-            .y = sin(2*kPi*i/nodeNumber),
-        });
-    }
-    std::vector<Edge> ev{};
-    for (auto i = sv.begin() + 1; i != sv.end(); ++i) {
-        stringstream edgeSS(*i);
-        auto edgeSV = split(edgeSS, ' ');
-        ev.push_back({
-            .start = std::stoul(edgeSV.at(0)),
-            .end = std::stoul(edgeSV.at(1)),
-        });
-    }
-    sg.nodes = nv;
-    sg.edges = ev;
-    InitGraphVisualizer(sg);
-    DrawGraph(sg);
-    while(timeLimit > 0) {
-        auto start = std::chrono::high_resolution_clock::now();
-
-        updateNodePositionForAttraction(sg);
-        updateNodePositionForRepulsive(sg);
-
-        auto end = std::chrono::high_resolution_clock::now();
-        auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
-        int millisecond = elapsed.count();
-        timeLimit -= (millisecond > 0 ? millisecond : 1);
-        std::cout << timeLimit << " " << millisecond << std::endl;
-    }
-    InitGraphVisualizer(sg);
-    DrawGraph(sg);
-    std::cout << "done" << std::endl;
     return 0;
 }
 
