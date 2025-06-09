3b9058fd0253e77dadb04a536bc56577866beb35
Vince Liao <vince.liao@nextbank.com.tw>
2025-06-09 14:52:17 +0800

    assignment02 part A done

--- assignment2/WikiRacerLinks/src/main.cpp
+++ assignment2/WikiRacerLinks/src/main.cpp
@@ -1,10 +1,29 @@
 #include <iostream>
 #include <fstream>
 #include <unordered_set>
+#include <sstream>
+#include <algorithm>
 
 using std::cout;            using std::endl;
 using std::string;          using std::unordered_set;
 
+string getFileName() {
+    cout << "Enter a file name: ";
+    string filename;
+    getline(std::cin, filename);
+    return filename;
+}
+
+string readFileIntoString(const string& filename) {
+    std::ifstream ifs("res/" + filename);
+    if (!ifs.is_open()) {
+        std::cout << "No such file" << std::endl;
+        return "";
+    }
+    std::stringstream ss;
+    ss << ifs.rdbuf();
+    return ss.str();
+}
 /*
  * Note that you have to pass in the file as a single string
  * into the findWikiLinks function!
@@ -12,32 +31,39 @@ using std::string;          using std::unordered_set;
  * does that sound familiar at all?
  */
 unordered_set<string> findWikiLinks(const string& page_html) {
-    // TODO: delete this return statement and implement
-    // the function!
-    return {};
+    unordered_set<string> pageNames;
+    const std::string wikiPattern = "\"/wiki/";
+    auto start = page_html.begin();
+    while (start != page_html.end()) {
+        auto startOfPattern = std::search(start, page_html.end(), wikiPattern.begin(), wikiPattern.end());
+        if (startOfPattern == page_html.end()) {
+            break;
+        }
+        auto startOfPageName = startOfPattern + wikiPattern.length();
+        auto endOfPageName = std::find(startOfPageName, page_html.end(), '"');
+
+        if (endOfPageName != page_html.end() && std::all_of(startOfPageName, endOfPageName, [](auto c) {
+            return c != ':' && c != '#';
+        })) {
+            pageNames.insert(string (startOfPageName, endOfPageName));
+        }
+        start = endOfPageName;
+    }
+    return pageNames;
 }
 
 int main() {
-    /* Note if your file reading isn't working, please go to the
-     * projects tab on the panel on the left, and in the run section,
-     * uncheck the "Run in terminal" box and re-check it. This
-     * should fix things.
-     */
-
-    cout << "Enter a file name: ";
-    string filename;
-    getline(std::cin, filename);
-
-    // TODO:
-    // - create a filestream from the filename, and convert
-    //   it into a single string of data
-    // - findWikiLinks
-    // - print out the links you found, and use the provided
-    //   test files under "Other files/res" to check whether
-    //   your findWikiLinks function is working correctly!
-    //
-    // You got this!
-
-    // Write code here
+    string page_html;
+    while (true) {
+        page_html = readFileIntoString(getFileName());
+        if (!page_html.empty()) {
+            break;
+        }
+    }
+    auto pageNames = findWikiLinks(page_html);
+    for (auto& pageName : pageNames) {
+        cout << pageName << endl;
+    }
+    return 0;
 
 }
